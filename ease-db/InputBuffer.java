e0b3647d8e7828ae0426ff9069c7ad673b3aa72e
HarshKorde5 <harshkorde05@gmail.com>
2025-12-28 16:30:14 +0530

    add: sample InputBuffer for REPL

---
+++ ease-db/InputBuffer.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class InputBuffer {
+
+    public static void printPrompt(){
+        System.out.print("db >> ");
+    }
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while(true){
+            printPrompt();
+
+            String input;
+            try{
+                input = reader.readLine();
+                if(input == null){
+                    System.exit(0);
+                }
+            }catch(IOException e){
+                System.err.println("Error reading input");
+                return;
+            }
+
+
+            if(input.equals(".exit")){
+                System.exit(0);
+            }else{
+                System.out.println("Unrecognized command '"+input+"'.");
+            }
+        }
+
+    }
+}
