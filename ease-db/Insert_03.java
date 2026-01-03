7212e3ed7839976a1854b77bf79faf7d4a916033
HarshKorde5 <harshkorde05@gmail.com>
2026-01-03 22:20:55 +0530

    add: INSERT statement validations in separate method isInsertValid() and defined MAX_ROWS

---
+++ ease-db/Insert_03.java
@@ -0,0 +1,175 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+enum MetaCommandResult {
+    SUCCESS,
+    UNRECOGNIZED_COMMAND
+}
+
+enum PrepareResult {
+    SUCCESS,
+    UNRECOGNIZED_STATEMENT
+}
+
+enum StatementType {
+    INSERT,
+    SELECT,
+}
+
+class Statement {
+    StatementType type;
+}
+
+class Row {
+    int id;
+    String name;
+    String email;
+}
+
+class Table {
+    List<Row> rows = new ArrayList<>();
+}
+
+class Insert_03 {
+    static Table table = new Table();
+    static final int MAX_ROWS = 100;
+
+    public static void printPrompt() {
+        System.out.print("ease-db> ");
+    }
+
+    public static MetaCommandResult doMetaCommand(String input) {
+        if (input.equals(".exit")) {
+            System.exit(0);
+            return MetaCommandResult.SUCCESS; // dead code : not reached after exit
+        } else {
+            return MetaCommandResult.UNRECOGNIZED_COMMAND;
+        }
+    }
+
+    public static PrepareResult prepareStatement(String input, Statement statement) {
+        input = input.toLowerCase();
+        if (input.startsWith("insert")) {
+            statement.type = StatementType.INSERT;
+            return PrepareResult.SUCCESS;
+        }
+
+        if (input.equals("select")) {
+            statement.type = StatementType.SELECT;
+            return PrepareResult.SUCCESS;
+        }
+
+        return PrepareResult.UNRECOGNIZED_STATEMENT;
+    }
+
+    public static void replLoop(BufferedReader reader) throws IOException {
+        while (true) {
+            printPrompt();
+            String input = reader.readLine();
+
+            if (input == null) {
+                break;
+            }
+
+            if (input.startsWith(".")) {
+                switch (doMetaCommand(input)) {
+                    case SUCCESS:
+                        continue;
+                    case UNRECOGNIZED_COMMAND:
+                        System.out.println("Unrecognized command '" + input + "'.");
+                        continue;
+                }
+            }
+
+            Statement statement = new Statement();
+            switch (prepareStatement(input, statement)) {
+                case SUCCESS:
+                    break;
+
+                case UNRECOGNIZED_STATEMENT:
+                    System.out.println("Unrecognized keyword at start of '" + input + "'.");
+                    continue;
+            }
+
+            executeStatement(statement, input);
+        }
+    }
+
+    public static boolean isInsertValid(String[] splitStatement) {
+        if (splitStatement.length < 4) {
+            System.out.println("Syntax Error. Usage: insert <id> <name> <email>");
+            return false;
+        }
+
+        try {
+            Integer.parseInt(splitStatement[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("ID must be a number.");
+            return false;
+        }
+
+        return true;
+
+    }
+
+    static boolean idExists(int id){
+        for(Row r: table.rows){
+            if(r.id == id){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void executeStatement(Statement statement, String input) {
+        switch (statement.type) {
+            case INSERT:
+                String[] statementSplit = input.split(" ", 4);
+
+                if (!isInsertValid(statementSplit)) {
+                    return;
+                }
+
+                if(table.rows.size() >= MAX_ROWS){
+                    System.out.println("Error: Table full.");
+                    return;
+                }
+                
+                int id = Integer.parseInt(statementSplit[1]);
+                if(idExists(id)){
+                    System.out.println("Error: Duplicate ID. Record with "+id+" already exists.");
+                    return;
+                }
+
+                Row row = new Row();
+                row.id = id;
+                row.name = statementSplit[2];
+                row.email = statementSplit[3];
+                table.rows.add(row);
+                System.out.println("Inserted 1 row.\nExecuted.\n");
+                break;
+            case SELECT:
+                System.out.println("id\t| name\t| email");
+                for( Row r : table.rows){
+                    System.out.println(r.id + "\t| "+r.name+"\t| "+r.email);
+                }
+                break;
+            default:
+                System.out.println("Won't reach here, but this is default unrecognized statement.");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            replLoop(reader);
+        } catch (IOException e) {
+            System.err.println("Error reading input.");
+        }
+    }
+}
